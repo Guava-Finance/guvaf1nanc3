502f292fc7b9d10205988116c7ddcc9daed5f315
mikeeolar <mikeola758@gmail.com>
2025-04-09 02:13:45 +0100

    feat(dashboard): fix statussystemOverlayStyle

--- lib/core/styles/theme/theme.dart
+++ lib/core/styles/theme/theme.dart
@@ -4,7 +4,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:guava/widgets/utility_widget.dart';
 
 ThemeData theme(BuildContext context) => ThemeData(
-      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      // colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
       useMaterial3: true,
       scaffoldBackgroundColor: hexColor('#28443F'),
       brightness: Brightness.light,
@@ -12,12 +12,12 @@ ThemeData theme(BuildContext context) => ThemeData(
       appBarTheme: AppBarTheme(
         elevation: 0,
         systemOverlayStyle: SystemUiOverlayStyle(
-            statusBarColor: Colors.transparent,
-            statusBarBrightness: Brightness.light,
-            statusBarIconBrightness: Brightness.light,
-            systemNavigationBarColor: Colors.white,
-            systemNavigationBarIconBrightness: Brightness.light),
-        color: Colors.transparent,
+          statusBarColor: Colors.transparent,
+          statusBarBrightness: Brightness.light,
+          statusBarIconBrightness: Brightness.light,
+          systemNavigationBarColor: Colors.white,
+          systemNavigationBarIconBrightness: Brightness.light,
+        ),
         iconTheme: const IconThemeData(color: Colors.white),
       ),
       primaryTextTheme: TextTheme(
