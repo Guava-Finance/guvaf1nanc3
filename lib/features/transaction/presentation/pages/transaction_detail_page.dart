e841be69734795f076f8004b8124ac124835bd5f
Oghenekparobor Eminokanju <robor.eminokanju@gmail.com>
2025-04-23 15:34:33 +0100

    chore: txn cleanup

--- lib/features/transaction/presentation/pages/transaction_detail_page.dart
+++ lib/features/transaction/presentation/pages/transaction_detail_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
 import 'package:guava/core/resources/extensions/context.dart';
+import 'package:guava/core/resources/extensions/widget.dart';
 import 'package:guava/core/routes/router.dart';
 import 'package:guava/core/styles/colors.dart';
 import 'package:guava/features/transaction/presentation/pages/sub/transaction_detail.dart';
@@ -15,45 +16,60 @@ class TransactionDetailPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return BackWrapper(
-      title: 'Transaction details',
-      trailing: Text(
-        'Help?',
-        style: context.textTheme.bodyMedium!.copyWith(
-          fontSize: 14.sp,
-          fontWeight: FontWeight.w600,
-          color: BrandColors.washedTextColor,
-        ),
-      ),
-      child: SingleChildScrollView(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                20.verticalSpace,
-                TransactionFee(),
-                20.verticalSpace,
-                TransactionDetail(),
-                20.verticalSpace,
-                CustomButton(
-                  title: 'Share receipt',
-                  onTap: () => context.push(
-                    pPaymentStatus,
-                  ),
-                )
-              ],
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Transaction details'),
+        actions: [
+          Text(
+            'Help?',
+            style: context.textTheme.bodyMedium!.copyWith(
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w600,
+              color: BrandColors.washedTextColor,
             ),
-            Positioned(
-              top: 5,
-              left: 0,
-              right: 0,
-              child: CircleAvatar(),
-            ),
-          ],
-        ),
+          ),
+          16.horizontalSpace,
+        ],
       ),
+      body: SafeArea(
+        child: SingleChildScrollView(
+          physics: ClampingScrollPhysics(),
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  40.verticalSpace,
+                  TransactionFee(),
+                  20.verticalSpace,
+                  TransactionDetail(),
+                  20.verticalSpace,
+                ],
+              ),
+              Positioned(
+                top: 25,
+                left: 0,
+                right: 0,
+                child: CircleAvatar(),
+              ),
+            ],
+          ),
+        ).padHorizontal,
+      ),
+      bottomNavigationBar: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          5.verticalSpace,
+          CustomButton(
+            title: 'Share receipt',
+            onTap: () => context.push(
+              pPaymentStatus,
+            ),
+          ),
+          20.verticalSpace,
+        ],
+      ).padHorizontal,
     );
   }
 }
