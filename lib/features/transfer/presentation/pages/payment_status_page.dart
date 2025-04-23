f511b6eee854a238cf1f2676c12ca6db8a0dcbd1
Oghenekparobor Eminokanju <robor.eminokanju@gmail.com>
2025-04-23 16:25:14 +0100

    chore: apyment status adjustement done

--- lib/features/transfer/presentation/pages/payment_status_page.dart
+++ lib/features/transfer/presentation/pages/payment_status_page.dart
@@ -5,6 +5,7 @@ import 'package:go_router/go_router.dart';
 import 'package:guava/const/resource.dart';
 import 'package:guava/core/resources/extensions/context.dart';
 import 'package:guava/core/resources/extensions/double.dart';
+import 'package:guava/core/resources/extensions/widget.dart';
 import 'package:guava/core/routes/router.dart';
 import 'package:guava/core/styles/colors.dart';
 import 'package:guava/features/transfer/presentation/pages/sub/payment_detail.dart';
@@ -17,82 +18,87 @@ class PaymentStatusPage extends ConsumerWidget {
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    return BackWrapper(
-      title: '',
-      hasBackButton: false,
-      trailing: Text(
-        'Done',
-        style: context.textTheme.bodyMedium!.copyWith(
-          fontSize: 14.sp,
-          fontWeight: FontWeight.w600,
-          color: BrandColors.washedTextColor,
-        ),
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          20.verticalSpace,
-          Center(
-            child: Column(
-              children: [
-                Image.asset(
-                  R.ASSETS_IMAGES_CHECKMARK_PNG,
-                  height: 32.h,
-                ),
-                5.verticalSpace,
-                Text(
-                  'Transfer successful',
-                  style: context.textTheme.bodyMedium!.copyWith(
-                    fontSize: 14.sp,
-                    fontWeight: FontWeight.w500,
-                    color: BrandColors.light,
-                  ),
-                ),
-                10.verticalSpace,
-                Text.rich(
-                  TextSpan(
-                    children: [
-                      TextSpan(
-                        text: currencyFormatter().format(5000.89),
-                        style: context.textTheme.bodyMedium?.copyWith(
-                          color: BrandColors.textColor,
-                          fontSize: 28.sp,
-                          fontWeight: FontWeight.w600,
-                        ),
-                      ),
-                      TextSpan(
-                        text: (5000.89).formatDecimal,
-                        style: context.textTheme.bodyMedium?.copyWith(
-                            color: BrandColors.washedTextColor,
-                            fontSize: 28.sp,
-                            fontWeight: FontWeight.w600),
-                      ),
-                    ],
-                  ),
-                ),
-                20.verticalSpace,
-              ],
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          Text(
+            'Done',
+            style: context.textTheme.bodyMedium!.copyWith(
+              fontSize: 14.sp,
+              fontWeight: FontWeight.w600,
+              color: BrandColors.washedTextColor,
             ),
           ),
-          PaymentDetail(),
-          20.verticalSpace,
-          Spacer(),
-          CustomButton(
-            title: 'Add to beneficiary',
-            onTap: () => context.push(
-              pPaymentStatus,
-            ),
-            backgroundColor: BrandColors.containerColor,
-            textColor: BrandColors.washedTextColor,
-          ),
-          10.verticalSpace,
-          CustomButton(
-            title: 'Share receipt',
-            onTap: () => context.push(
-              pPaymentStatus,
-            ),
-          )
+          16.horizontalSpace,
         ],
+      ),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            20.verticalSpace,
+            Center(
+              child: Column(
+                children: [
+                  Image.asset(
+                    R.ASSETS_IMAGES_CHECKMARK_PNG,
+                    height: 32.h,
+                  ),
+                  5.verticalSpace,
+                  Text(
+                    'Transfer successful',
+                    style: context.textTheme.bodyMedium!.copyWith(
+                      fontSize: 14.sp,
+                      fontWeight: FontWeight.w500,
+                      color: BrandColors.light,
+                    ),
+                  ),
+                  10.verticalSpace,
+                  Text.rich(
+                    TextSpan(
+                      children: [
+                        TextSpan(
+                          text: currencyFormatter().format(5000.89),
+                          style: context.textTheme.bodyMedium?.copyWith(
+                            color: BrandColors.textColor,
+                            fontSize: 28.sp,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        TextSpan(
+                          text: (5000.89).formatDecimal,
+                          style: context.textTheme.bodyMedium?.copyWith(
+                              color: BrandColors.washedTextColor,
+                              fontSize: 28.sp,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ],
+                    ),
+                  ),
+                  20.verticalSpace,
+                ],
+              ),
+            ),
+            PaymentDetail(),
+            20.verticalSpace,
+            Spacer(),
+            CustomButton(
+              title: 'Add to beneficiary',
+              onTap: () => context.push(
+                pPaymentStatus,
+              ),
+              backgroundColor: BrandColors.containerColor,
+              textColor: BrandColors.washedTextColor,
+            ),
+            10.verticalSpace,
+            CustomButton(
+              title: 'Share receipt',
+              onTap: () => context.push(
+                pPaymentStatus,
+              ),
+            )
+          ],
+        ).padHorizontal,
       ),
     );
   }
